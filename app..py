e69a9a3da4beef4fd587628bd403a47d898f3c91
olododo omotomiwa <omotomiwar@gmail.com>
2023-09-22 22:55:14 +0100

    first commit in weater app

---
+++ app..py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, redirect
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'weatherapp'
+
+@app.route('/',  method = ['GET', 'POST'])
+def get_weather_app():
+    pass
+
+if __name__ == ('__main__'):
+    app.run(debug=True)
