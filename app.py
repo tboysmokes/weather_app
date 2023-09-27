8d31fa0b90daaf3b81b738bfbbe837025227e849
olododo omotomiwa <omotomiwar@gmail.com>
2023-09-27 19:39:28 +0100

    delete some code that aren't needed and add some features to display

--- app.py
+++ app.py
@@ -9,23 +9,27 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'weatherapp'
 
 
-# I set city to a defalt values incase the user doesn't input anything
+# I set city to a defalt value incase the user doesn't input anything
 def get_weather_data(city = "lagos"):
 
     request_url = f'https://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=imperial'
 
     weather_data = requests.get(request_url).json()
-
     print(weather_data)
+
 
     return weather_data
 
 
 @app.route('/',  methods = ['GET', 'POST'])
-@app.route('/index',  methods = ['GET', 'POST'])
 def get_data():
     if request.method == "POST":
         city = request.form['input']
+
+
+        if city == "":
+            city = "lagos"
+
 
         weather_data = get_weather_data(city)
 
@@ -34,9 +38,11 @@ def get_data():
             title = weather_data["name"],
             status = weather_data["weather"][0]["description"],
             temp = f"{weather_data['main']['temp']}",
-            feels_like = f"{weather_data['main']['feels_like']}"
+            feels_like = f"{weather_data['main']['feels_like']}",
+            min_temp = f"{weather_data['main']['temp_min']}",
+            max_temp = f"{weather_data['main']['temp_max']}",
         )
-    return render_template("interface.html")
+    return render_template("weather.html")
 
 if __name__ == ('__main__'):
     app.run(debug=True)
